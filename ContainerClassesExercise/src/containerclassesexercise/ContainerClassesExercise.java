4acf65d5313a135859387c0361257a1528cd620c
Fenerli Teekay <fenerlitk@gmail.com>
2012-05-22 16:49:34 +0100

    test

---
+++ ContainerClassesExercise/src/containerclassesexercise/ContainerClassesExercise.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package containerclassesexercise;
+
+/**
+ *
+ * @author fenerlitk
+ */
+public class ContainerClassesExercise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
